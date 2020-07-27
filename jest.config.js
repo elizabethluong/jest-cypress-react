c78bc72d3958a7376606a90376267d2fe22344b7
Elizabeth luong <elizabethluong@live.co.uk>
2020-07-27 13:17:35 +0100

    add identity-obj-proxy as mock css module

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy', //when modules matches modules.css end of file path it will map to identity-obj-proxy to return the string
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
 }
