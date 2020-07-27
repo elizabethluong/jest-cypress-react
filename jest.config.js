5643850fafe15a75c8bc57f3cb68ebe558160798
Elizabeth luong <elizabethluong@live.co.uk>
2020-07-27 15:08:23 +0100

    Run setup for all test with setupFilesAfterEnv in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy', //when modules matches modules.css end of file path it will map to identity-obj-proxy to return the string
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 }
